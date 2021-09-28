1585c548cf709a773091747d300fc384c4ad1a7b
Mahfuzislam1695 <mahfuzislam1695@gmail.com>
2021-09-29 04:20:49 +0600

    full

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 
 const Cart = (props) => {
     const cart = props.cart;
-    console.log(cart);
+    // console.log(cart);
     // const total = cart.reduce( (total, prd) => total + prd.price, 0)
     let total = 0;
     for (let i = 0; i < cart.length; i++) {
         const product = cart[i];
-        total = total + product.price;
+        total = total + product.price * product.quantity;
     }
     let shipping = 0;
     if (total > 35) {
@@ -36,9 +36,10 @@ const Cart = (props) => {
             <p><small>Tax+Vat: {tax}</small></p>
             {<p>Total Price: {grandTotal}</p>}
             <br />
-            <Link to='/review'>
-                <button className='main-button'>Review Order</button>
-            </Link>
+            {
+                props.children
+            }
+            
         </div>
     );
 };
